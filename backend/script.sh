d564c24fea29d2b7a3010f2bb928cf5bcaeb0ed9
Franchis Janel MOKOMBA <janelmokomba@gmail.com>
2024-07-14 19:14:33 +0000

    Initial commit

---
+++ backend/script.sh
@@ -0,0 +1,3 @@
+# Create dotenv file, on multiple lines
+# echo "PORT=1111" > .env
+# echo "API_SECRET=0987654321" >> .env
--- git.sh
+++ git.sh
@@ -1,5 +1,8 @@
+# Message varible argument
+message=$1
+
 # Initialize a git repository and push it to GitHub and Gitea
-git init
+# git init
 
 # Add all files to the repository
 git add .
@@ -8,16 +11,19 @@ git add .
 git status
 
 # Commit the files
-git commit -m "Initial commit"
+# git commit -m "Initial commit"
+git commit -m "$message"
 
 # Push the repository to GitHub
-git remote add origin1 https://github.com/pro12x/social-network.git
+# git remote add origin1 https://github.com/pro12x/social-network.git
 
 # Push the repository to Gitea
-git remote add origin2 https://learn.zone01dakar.sn/git/fmokomba/social-network.git
+# git remote add origin2 https://learn.zone01dakar.sn/git/fmokomba/social-network.git
 
 # Push the repository to both GitHub and Gitea
-git push -u origin1 master
-git push -u origin2 master
+# git push -u origin1 master
+git push origin1 master
+# git push -u origin2 master
+git push origin2 master
 
 # End of script
